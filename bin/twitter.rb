09c9ab18de4e8571b0ad2e40d7346cf9b1ecc583
Daniel Gamboa <danielgamboa10@gmail.com>
2012-11-03 20:16:50 -0700

    Session_handler is now capable of distributing new and existing challenges

--- bin/twitter.rb
+++ bin/twitter.rb
@@ -4,86 +4,98 @@ require 'tweetstream'
 require 'yaml'
 require_relative 'session'
 
-class TwitterConnectFour
-  # ----------------------------------------------------------------------------------------
-  # Twitter configuration
-  # ----------------------------------------------------------------------------------------
 
-  TWITTER_CONSUMER_KEY = "yR6hU0qcwt0vVd8GRwchXw"
-  TWITTER_CONSUMER_SECRET = "VQTgrwTonVYJQY4o1nS1ucAJ3eUILfOOhcUkKoLFko"
-  TWITTER_OAUTH_TOKEN = "921355608-LBZ9ASptzT0f9tAbCmcxJjREcjj82kMnMdzrrBbT"
-  TWITTER_OAUTH_TOKEN_SECRET = "jdsihj0ZNeTXZxaGEJ4h0yp6RUkj1SOMe8dGbaM20"
+# ----------------------------------------------------------------------------------------
+# Twitter configuration
+# ----------------------------------------------------------------------------------------
 
-  Twitter.configure do |config|
-      config.consumer_key       = TWITTER_CONSUMER_KEY
-      config.consumer_secret    = TWITTER_CONSUMER_SECRET
-      config.oauth_token        = TWITTER_OAUTH_TOKEN
-      config.oauth_token_secret = TWITTER_OAUTH_TOKEN_SECRET
-  end
+TWITTER_CONSUMER_KEY = "yR6hU0qcwt0vVd8GRwchXw"
+TWITTER_CONSUMER_SECRET = "VQTgrwTonVYJQY4o1nS1ucAJ3eUILfOOhcUkKoLFko"
+TWITTER_OAUTH_TOKEN = "921355608-LBZ9ASptzT0f9tAbCmcxJjREcjj82kMnMdzrrBbT"
+TWITTER_OAUTH_TOKEN_SECRET = "jdsihj0ZNeTXZxaGEJ4h0yp6RUkj1SOMe8dGbaM20"
 
-  TweetStream.configure do |config|
-      config.consumer_key       = TWITTER_CONSUMER_KEY
-      config.consumer_secret    = TWITTER_CONSUMER_SECRET
-      config.oauth_token        = TWITTER_OAUTH_TOKEN
-      config.oauth_token_secret = TWITTER_OAUTH_TOKEN_SECRET
-      config.auth_method        = :oauth
-  end
+Twitter.configure do |config|
+    config.consumer_key       = TWITTER_CONSUMER_KEY
+    config.consumer_secret    = TWITTER_CONSUMER_SECRET
+    config.oauth_token        = TWITTER_OAUTH_TOKEN
+    config.oauth_token_secret = TWITTER_OAUTH_TOKEN_SECRET
+end
 
-  # ----------------------------------------------------------------------------------------
-  # Methods used for Twitter game flow: these need to be defined before running TweetStream
-  # ----------------------------------------------------------------------------------------
+TweetStream.configure do |config|
+    config.consumer_key       = TWITTER_CONSUMER_KEY
+    config.consumer_secret    = TWITTER_CONSUMER_SECRET
+    config.oauth_token        = TWITTER_OAUTH_TOKEN
+    config.oauth_token_secret = TWITTER_OAUTH_TOKEN_SECRET
+    config.auth_method        = :oauth
+end
 
-  def post_challenge
-    post_id = Time.now.strftime("%F %T.%L")
-    Twitter.update("Who wants to get demolished? #dbc_c4 #{post_id}")
-    listen_for_game
-  end
+# ----------------------------------------------------------------------------------------
+# Methods used for Twitter game flow: these need to be defined before running TweetStream
+# ----------------------------------------------------------------------------------------
 
-  def challenge?(message)
-    return !(message =~ /[\s]*\@deepteal[\s]*[G|g]ame[\s]*on[!]?[\s]*\#dbc_c4[\s]*/).nil?
-  end
+$sessions_container = []
 
-  def board?(message)
-    return !(message =~ /[\s]*\@deepteal[\s]*|[.+]|[.+]|[.+]|[.+]|[.+]|[.+]|[\s]*\#dbc_c4[\s]*/).nil?
-  end
+def post_challenge
+  post_id = Time.now.strftime("%F %T.%L")
+  Twitter.update("Who wants to get demolished? #dbc_c4 #{post_id}")
+  listen_for_game
+end
 
-  def start_game(player)
-    Session.new(player)
-  end
+def challenge?(message)
+  return !(message =~ /[\s]*\@deepteal[\s]*[G|g]ame[\s]*on[!]?[\s]*\#dbc_c4[\s]*/).nil?
+end
 
-  def send_to_session(player, board)
+def board?(message)
+  return !(message =~ /[\s]*\@deepteal[\s]*(\|\S{7}\|\S{7}\|\S{7}\|\S{7}\|\S{7}\|\S{7}\|)[\s]*\#dbc_c4[\s]*/).nil?
+end
 
-  end
+def start_game(player)
+  $sessions_container << Session.new(player)
+end
 
-  # ----------------------------------------------------------------------------------------
-  # Start the game and open the TweetStream
-  # ----------------------------------------------------------------------------------------
+def strip_to_board(message)
+  message.match(/[\s]*\@deepteal[\s]*(\|\S{7}\|\S{7}\|\S{7}\|\S{7}\|\S{7}\|\S{7}\|)[\s]*\#dbc_c4[\s]*/)
+  return $1
+end
 
-  post_challenge
-
-  def listen_for_game
-    TweetStream::Client.new.on_delete{ |status_id, user_id|
-      Tweet.delete(status_id)
-      }.on_limit { |skip_count|
-        puts "skipping"
-        sleep 5
-      }.track('@deepteal') do |status|
-        msg = status.text
-        if challenge?(msg)
-          player = status.user.screen_name
-          Twitter.update("\@{player} get ready to be crushed!")
-          start_game(player)
-          post_challenge
-        elsif board?(msg)
-          player = status.user.screen_name
-          board = strip_to_board(msg)
-          send_to_session(player, board)
-          #needs to check if session exists
-          #  no? initiate a session
-          # yes? send board to right session
-        end
+def send_to_session(player, board)
+  $sessions_container.each do |session|
+    if session.player == player
+      session.receive(board)
+      return true
     end
+  end
+  false
+end
+
+# ----------------------------------------------------------------------------------------
+# Start the game and open the TweetStream
+# ----------------------------------------------------------------------------------------
+
+post_challenge
+
+def listen_for_game
+  TweetStream::Client.new.on_delete{ |status_id, user_id|
+    Tweet.delete(status_id)
+    }.on_limit { |skip_count|
+      puts "skipping"
+      sleep 5
+    }.track('@deepteal') do |status|
+      msg = status.text
+      if challenge?(msg)
+        player = status.user.screen_name
+        Twitter.update("\@{player} get ready to be crushed!")
+        start_game(player)
+        post_challenge
+      elsif board?(msg)
+        player = status.user.screen_name
+        board = strip_to_board(msg)
+        start_game(player) unless send_to_session(player, board)
+      end
   end
 end
 
-TwitterGame.new
+# A session object should have a player attr_reader that containes the opponent's username (for session_handler check and to know who to send to)
+# It should also have a method #receive that takes in a board in string format
+# It should also have an initialize method that takes in a player in the format 'username' not '@username'
+
